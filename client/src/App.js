32ea5280fce344948adc68f0e8bd66e323b4dcb7
Melanie <melaniechele@gmail.com>
2020-09-02 21:19:29 -0400

    started project

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import {Route} from 'reacr-rounter-dom'; 
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
